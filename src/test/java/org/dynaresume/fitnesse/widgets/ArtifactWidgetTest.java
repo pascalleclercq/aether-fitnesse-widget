aefdf1fe3d1688be1457fbe221131bbe72225f03
pascalleclercq <pascal.leclercq@gmail.com>
2010-11-13 22:36:12 +0100

    Unit tests updated

--- src/test/java/org/dynaresume/fitnesse/widgets/ArtifactWidgetTest.java
+++ src/test/java/org/dynaresume/fitnesse/widgets/ArtifactWidgetTest.java
@@ -22,7 +22,7 @@ public class ArtifactWidgetTest extends TestCase {
 		String resolvedClassPath = artifactWidget.childHtml();
 		System.out.println(resolvedClassPath);
 
-		assertEquals(artifactWidget.getLocalRepo() + "/junit/junit/3.8.2/junit-3.8.2.jar", resolvedClassPath);
+		assertEquals(artifactWidget.getVariable("LOCAL_REPO") + "/junit/junit/3.8.2/junit-3.8.2.jar", resolvedClassPath);
 
 	}
 
@@ -34,7 +34,7 @@ public class ArtifactWidgetTest extends TestCase {
 		String resolvedClassPath = artifactWidget.childHtml();
 		System.out.println(resolvedClassPath);
 
-		assertEquals(artifactWidget.getLocalRepo() + "/junit/junit/4.8.2/junit-4.8.2.jar", resolvedClassPath);
+		assertEquals(artifactWidget.getVariable("LOCAL_REPO") + "/junit/junit/4.8.2/junit-4.8.2.jar", resolvedClassPath);
 
 	}
 
@@ -46,15 +46,15 @@ public class ArtifactWidgetTest extends TestCase {
 		String resolvedClassPath = artifactWidget.childHtml();
 		System.out.println(resolvedClassPath);
 		assertEquals(
-				artifactWidget.getLocalRepo() + "/org/hibernate/hibernate-core/3.3.0.CR1/hibernate-core-3.3.0.CR1.jar:"
-						+ artifactWidget.getLocalRepo() + "/antlr/antlr/2.7.6/antlr-2.7.6.jar:" + artifactWidget.getLocalRepo()
-						+ "/commons-collections/commons-collections/3.1/commons-collections-3.1.jar:" + artifactWidget.getLocalRepo()
-						+ "/dom4j/dom4j/1.6.1/dom4j-1.6.1.jar:" + artifactWidget.getLocalRepo()
-						+ "/xml-apis/xml-apis/1.0.b2/xml-apis-1.0.b2.jar:" + artifactWidget.getLocalRepo()
-						+ "/javax/transaction/jta/1.1/jta-1.1.jar:" + artifactWidget.getLocalRepo()
-						+ "/javassist/javassist/3.4.GA/javassist-3.4.GA.jar:" + artifactWidget.getLocalRepo()
-						+ "/cglib/cglib/2.1_3/cglib-2.1_3.jar:" + artifactWidget.getLocalRepo() + "/asm/asm/1.5.3/asm-1.5.3.jar:"
-						+ artifactWidget.getLocalRepo() + "/asm/asm-attrs/1.5.3/asm-attrs-1.5.3.jar:" + artifactWidget.getLocalRepo()
+				artifactWidget.getVariable("LOCAL_REPO") + "/org/hibernate/hibernate-core/3.3.0.CR1/hibernate-core-3.3.0.CR1.jar:"
+						+ artifactWidget.getVariable("LOCAL_REPO") + "/antlr/antlr/2.7.6/antlr-2.7.6.jar:" + artifactWidget.getVariable("LOCAL_REPO")
+						+ "/commons-collections/commons-collections/3.1/commons-collections-3.1.jar:" + artifactWidget.getVariable("LOCAL_REPO")
+						+ "/dom4j/dom4j/1.6.1/dom4j-1.6.1.jar:" + artifactWidget.getVariable("LOCAL_REPO")
+						+ "/xml-apis/xml-apis/1.0.b2/xml-apis-1.0.b2.jar:" + artifactWidget.getVariable("LOCAL_REPO")
+						+ "/javax/transaction/jta/1.1/jta-1.1.jar:" + artifactWidget.getVariable("LOCAL_REPO")
+						+ "/javassist/javassist/3.4.GA/javassist-3.4.GA.jar:" + artifactWidget.getVariable("LOCAL_REPO")
+						+ "/cglib/cglib/2.1_3/cglib-2.1_3.jar:" + artifactWidget.getVariable("LOCAL_REPO") + "/asm/asm/1.5.3/asm-1.5.3.jar:"
+						+ artifactWidget.getVariable("LOCAL_REPO") + "/asm/asm-attrs/1.5.3/asm-attrs-1.5.3.jar:" + artifactWidget.getVariable("LOCAL_REPO")
 						+ "/org/slf4j/slf4j-api/1.4.2/slf4j-api-1.4.2.jar", resolvedClassPath);
 
 	}
