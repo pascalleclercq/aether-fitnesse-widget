68cf1e1b8e3383e9961b2dbfed5221d8ca2fe897
pascal leclercq <pascal.leclercq@gmail.com>
2011-10-19 00:27:50 +0200

    update license header to CPL....

--- src/main/java/fr/opensagres/fitnesse/widgets/ArtifactWidget.java
+++ src/main/java/fr/opensagres/fitnesse/widgets/ArtifactWidget.java
@@ -1,14 +1,6 @@
-/*
- * Copyright (c) 2010 Pascal Leclercq. All rights reserved.
- *
- * This program is licensed to you under the Apache License Version 2.0, 
- * and you may not use this file except in compliance with the Apache License Version 2.0. 
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the Apache License Version 2.0 is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+/**
+ * // Copyright (C) 2003-2009 by Object Mentor, Inc. All rights reserved.
+ * // Released under the terms of the CPL Common Public License version 1.0.
  */
 package fr.opensagres.fitnesse.widgets;
 
--- src/main/java/fr/opensagres/fitnesse/widgets/internal/ManualRepositorySystemFactory.java
+++ src/main/java/fr/opensagres/fitnesse/widgets/internal/ManualRepositorySystemFactory.java
@@ -1,3 +1,7 @@
+/**
+ * // Copyright (C) 2003-2009 by Object Mentor, Inc. All rights reserved.
+ * // Released under the terms of the CPL Common Public License version 1.0.
+ */
 package fr.opensagres.fitnesse.widgets.internal;
 /*******************************************************************************
  * Copyright (c) 2010-2011 Sonatype, Inc.
