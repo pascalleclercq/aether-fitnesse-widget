a89b2205dc022a975200eb55720f0fe165fa6412
pascal leclercq <pascal.leclercq@gmail.com>
2013-03-29 00:42:45 +0100

    OPEN - task 3: upgrade versions https://github.com/pascalleclercq/aether-fitnesse-widget/issues/issue/3
    cleaup

--- src/test/java/fr/opensagres/fitnesse/widgets/ClassPathBuilderTest.java
+++ src/test/java/fr/opensagres/fitnesse/widgets/ClassPathBuilderTest.java
@@ -7,10 +7,8 @@ import java.io.File;
 import java.util.List;
 import java.util.Properties;
 
-import util.FileUtil;
 import util.RegexTestCase;
 import fitnesse.ComponentFactory;
-import fitnesse.components.ClassPathBuilder;
 import fitnesse.wiki.InMemoryPage;
 import fitnesse.wiki.PageCrawler;
 import fitnesse.wiki.PageData;
@@ -18,191 +16,28 @@ import fitnesse.wiki.PathParser;
 import fitnesse.wiki.WikiPage;
 
 public class ClassPathBuilderTest extends RegexTestCase {
-	private static final String TEST_DIR = "testDir";
-	public static void deleteSampleFiles() {
-		FileUtil.deleteFileSystemDirectory(TEST_DIR);
-	}
-	public static void makeSampleFiles() {
-		FileUtil.makeDir(TEST_DIR);
-		FileUtil.createFile(TEST_DIR + "/one.jar", "");
-		FileUtil.createFile(TEST_DIR + "/two.jar", "");
-		FileUtil.createFile(TEST_DIR + "/one.dll", "");
-		FileUtil.createFile(TEST_DIR + "/two.dll", "");
-		FileUtil.createFile(TEST_DIR + "/oneA", "");
-		FileUtil.createFile(TEST_DIR + "/twoA", "");
-		FileUtil.createDir(TEST_DIR + "/subdir");
-		FileUtil.createFile(TEST_DIR + "/subdir/sub1.jar", "");
-		FileUtil.createFile(TEST_DIR + "/subdir/sub2.jar", "");
-		FileUtil.createFile(TEST_DIR + "/subdir/sub1.dll", "");
-		FileUtil.createFile(TEST_DIR + "/subdir/sub2.dll", "");
-	}
-	private ClassPathBuilder builder;
+	
 
 	private PageCrawler crawler;
 
 	private ComponentFactory factory;
 
-//	public void testGetClasspath() throws Exception {
-//		crawler.addPage(root, PathParser.parse("TestPage"), "!path fitnesse.jar\n" + "!path my.jar");
-//		String expected = "fitnesse.jar" + pathSeparator + "my.jar";
-//		assertEquals(expected, builder.getClasspath(root.getChildPage("TestPage")));
-//	}
-//
-//	public void testPathSeparatorVariable() throws Exception {
-//		WikiPage page = crawler.addPage(root, PathParser.parse("TestPage"), "!define PATH_SEPARATOR {|}\n" + "!path fitnesse.jar\n" + "!path my.jar");
-//		PageData data = page.getData();
-//		page.commit(data);
-//
-//		String expected = "fitnesse.jar" + "|" + "my.jar";
-//		assertEquals(expected, builder.getClasspath(root.getChildPage("TestPage")));
-//	}
-//
-//	public void testGetPaths_OneLevel() throws Exception {
-//		String pageContent = "This is some content\n" + "!path aPath\n" + "end of conent\n";
-//		WikiPage root = InMemoryPage.makeRoot("RooT");
-//		WikiPage page = crawler.addPage(root, PathParser.parse("ClassPath"), pageContent);
-//		String path = builder.getClasspath(page);
-//		assertEquals("aPath", path);
-//	}
-//
-//	public void testGetClassPathMultiLevel() throws Exception {
-//		WikiPage root = InMemoryPage.makeRoot("RooT");
-//		crawler.addPage(root, PathParser.parse("ProjectOne"), "!path path2\n" + "!path path 3");
-//		crawler.addPage(root, PathParser.parse("ProjectOne.TesT"), "!path path1");
-//
-//		String cp = builder.getClasspath(crawler.getPage(root, PathParser.parse("ProjectOne.TesT")));
-//		assertSubString("path1", cp);
-//		assertSubString("path2", cp);
-//		assertSubString("\"path 3\"", cp);
-//	}
-//
-//	public void testLinearClassPath() throws Exception {
-//		WikiPage root = InMemoryPage.makeRoot("RooT");
-//		WikiPage superPage = crawler.addPage(root, PathParser.parse("SuperPage"), "!path superPagePath");
-//		WikiPage subPage = crawler.addPage(superPage, PathParser.parse("SubPage"), "!path subPagePath");
-//		String cp = builder.getClasspath(subPage);
-//		assertEquals("subPagePath" + pathSeparator + "superPagePath", cp);
-//
-//	}
-//
-//	public void testGetClassPathFromPageThatDoesntExist() throws Exception {
-//		String classPath = makeClassPathFromSimpleStructure("somePath");
-//
-//		assertEquals("somePath", classPath);
-//	}
-//
-//	private String makeClassPathFromSimpleStructure(String path) throws Exception {
-//		PageData data = root.getData();
-//		data.setContent("!path " + path);
-//		root.commit(data);
-//		crawler = root.getPageCrawler();
-//		crawler.setDeadEndStrategy(new MockingPageCrawler());
-//		WikiPage page = crawler.getPage(root, somePagePath);
-//		String classPath = builder.getClasspath(page);
-//		return classPath;
-//	}
-//
-//	public void testThatPathsWithSpacesGetQuoted() throws Exception {
-//		crawler.addPage(root, somePagePath, "!path Some File.jar");
-//		crawler = root.getPageCrawler();
-//		WikiPage page = crawler.getPage(root, somePagePath);
-//
-//		assertEquals("\"Some File.jar\"", builder.getClasspath(page));
-//
-//		crawler.addPage(root, somePagePath, "!path somefile.jar\n!path Some Dir/someFile.jar");
-//		assertEquals("somefile.jar" + pathSeparator + "\"Some Dir/someFile.jar\"", builder.getClasspath(page));
-//	}
-//
-//	public void testWildCardExpansion() throws Exception {
-//		try {
-//			makeSampleFiles();
-//
-//			String classPath = makeClassPathFromSimpleStructure("testDir/*.jar");
-//			assertHasRegexp("one\\.jar", classPath);
-//			assertHasRegexp("two\\.jar", classPath);
-//
-//			classPath = makeClassPathFromSimpleStructure("testDir/*.dll");
-//			assertHasRegexp("one\\.dll", classPath);
-//			assertHasRegexp("two\\.dll", classPath);
-//
-//			classPath = makeClassPathFromSimpleStructure("testDir/one*");
-//			assertHasRegexp("one\\.dll", classPath);
-//			assertHasRegexp("one\\.jar", classPath);
-//			assertHasRegexp("oneA", classPath);
-//
-//			classPath = makeClassPathFromSimpleStructure("testDir/**.jar");
-//			assertHasRegexp("one\\.jar", classPath);
-//			assertHasRegexp("two\\.jar", classPath);
-//			assertHasRegexp("subdir(?:\\\\|/)sub1\\.jar", classPath);
-//			assertHasRegexp("subdir(?:\\\\|/)sub2\\.jar", classPath);
-//		} finally {
-//			deleteSampleFiles();
-//		}
-//	}
-
-	String pathSeparator = System.getProperty("path.separator");
+	private String repoDir = new File(ClassPathBuilderTest.class.getResource("/").getFile()).getParent() + "/testRepo";
 
 	private WikiPage root;
-
 	private Properties testProperties;
+
+
 	public void setUp() throws Exception {
 		testProperties = new Properties();
 		String symbolValues = MavenArtifact.class.getName();
 		testProperties.setProperty(ComponentFactory.SYMBOL_TYPES, symbolValues);
-		// testProvider = new SymbolProvider(new SymbolType[] {MavenArtifact.symbolType});
-
 		factory = new ComponentFactory(testProperties);
-		
-		String output = factory.loadSymbolTypes();
-		System.out.println(output);
-
+		factory.loadSymbolTypes();
 		root = InMemoryPage.makeRoot("RooT");
-
 		crawler = root.getPageCrawler();
-		builder = new ClassPathBuilder();
-
-
-	}
-
-
-	public void testJunit382() throws Exception {
-		// Very simple test : only 1 dependency resolved, jar is a dependency of
-		// the current module
-		WikiPage page = crawler.addPage(root, PathParser.parse("TestPage"), "!artifact junit:junit:3.8.2\n" + "!path my.jar");
-		PageData data = page.getData();
-		page.commit(data);
-
-		//System.out.println(builder.getClasspath(root.getChildPage("TestPage")));
-
-		List<String> paths = page.getData().getClasspaths();
-		
-		for (String path : paths) {
-			System.out.println("XXXXXXX   "+path);
-		}
-		assertEquals(MavenArtifact.userMavenConfigurationHome + "/repository" + "/junit/junit/3.8.2/junit-3.8.2.jar", paths.get(0));
-
 	}
 	
-	public void testJunit382AlternateSettings() throws Exception {
-		// Very simple test : only 1 dependency resolved, jar is a dependency of
-		// the current module
-		WikiPage page = crawler.addPage(root, PathParser.parse("TestPage2"), "!define settings {src/test/resources/settings.xml}\n"+"!artifact junit:junit:3.8.2\n" + "!path my.jar");
-		PageData data = page.getData();
-		page.commit(data);
-
-	//	System.out.println(builder.getClasspath(root.getChildPage("TestPage2")));
-
-		List<String> paths = page.getData().getClasspaths();
-	//	System.out.println(paths.size());
-		
-		assertEquals( repoDir +"/junit/junit/3.8.2/junit-3.8.2.jar",
-				paths.get(0));
-
-
-	}
-	
-	
-	private String repoDir = new File(ClassPathBuilderTest.class.getResource("/").getFile()).getParent() + "/testRepo";
 	public void testComplexDependency() throws Exception {
 		// Complex test : Full tree resolved from http://repository.jboss.org/maven2/
 		WikiPage page = crawler.addPage(root, PathParser.parse("TestPage3"), 
@@ -228,8 +63,138 @@ public class ClassPathBuilderTest extends RegexTestCase {
 	}
 	
 	
+	public void testJunit382() throws Exception {
+		// Very simple test : only 1 dependency resolved, jar is a dependency of
+		// the current module
+		WikiPage page = crawler.addPage(root, PathParser.parse("TestPage"), "!artifact junit:junit:3.8.2\n" + "!path my.jar");
+		PageData data = page.getData();
+		page.commit(data);
+
+		List<String> paths = page.getData().getClasspaths();
+		
+		assertEquals(MavenArtifact.userMavenConfigurationHome + "/repository" + "/junit/junit/3.8.2/junit-3.8.2.jar", paths.get(0));
+
+	}
+	public void testJunit382AlternateSettings() throws Exception {
+		// Very simple test : only 1 dependency resolved, jar is a dependency of
+		// the current module
+		WikiPage page = crawler.addPage(root, PathParser.parse("TestPage2"), "!define settings {src/test/resources/settings.xml}\n"+"!artifact junit:junit:3.8.2\n" + "!path my.jar");
+		PageData data = page.getData();
+		page.commit(data);
+
+	//	System.out.println(builder.getClasspath(root.getChildPage("TestPage2")));
+
+		List<String> paths = page.getData().getClasspaths();
+	//	System.out.println(paths.size());
+		
+		assertEquals( repoDir +"/junit/junit/3.8.2/junit-3.8.2.jar",
+				paths.get(0));
+
+
+	}
+	
+	
 	
 	
 	
 	
 }
+
+
+//public void testGetClasspath() throws Exception {
+//	crawler.addPage(root, PathParser.parse("TestPage"), "!path fitnesse.jar\n" + "!path my.jar");
+//	String expected = "fitnesse.jar" + pathSeparator + "my.jar";
+//	assertEquals(expected, builder.getClasspath(root.getChildPage("TestPage")));
+//}
+//
+//public void testPathSeparatorVariable() throws Exception {
+//	WikiPage page = crawler.addPage(root, PathParser.parse("TestPage"), "!define PATH_SEPARATOR {|}\n" + "!path fitnesse.jar\n" + "!path my.jar");
+//	PageData data = page.getData();
+//	page.commit(data);
+//
+//	String expected = "fitnesse.jar" + "|" + "my.jar";
+//	assertEquals(expected, builder.getClasspath(root.getChildPage("TestPage")));
+//}
+//
+//public void testGetPaths_OneLevel() throws Exception {
+//	String pageContent = "This is some content\n" + "!path aPath\n" + "end of conent\n";
+//	WikiPage root = InMemoryPage.makeRoot("RooT");
+//	WikiPage page = crawler.addPage(root, PathParser.parse("ClassPath"), pageContent);
+//	String path = builder.getClasspath(page);
+//	assertEquals("aPath", path);
+//}
+//
+//public void testGetClassPathMultiLevel() throws Exception {
+//	WikiPage root = InMemoryPage.makeRoot("RooT");
+//	crawler.addPage(root, PathParser.parse("ProjectOne"), "!path path2\n" + "!path path 3");
+//	crawler.addPage(root, PathParser.parse("ProjectOne.TesT"), "!path path1");
+//
+//	String cp = builder.getClasspath(crawler.getPage(root, PathParser.parse("ProjectOne.TesT")));
+//	assertSubString("path1", cp);
+//	assertSubString("path2", cp);
+//	assertSubString("\"path 3\"", cp);
+//}
+//
+//public void testLinearClassPath() throws Exception {
+//	WikiPage root = InMemoryPage.makeRoot("RooT");
+//	WikiPage superPage = crawler.addPage(root, PathParser.parse("SuperPage"), "!path superPagePath");
+//	WikiPage subPage = crawler.addPage(superPage, PathParser.parse("SubPage"), "!path subPagePath");
+//	String cp = builder.getClasspath(subPage);
+//	assertEquals("subPagePath" + pathSeparator + "superPagePath", cp);
+//
+//}
+//
+//public void testGetClassPathFromPageThatDoesntExist() throws Exception {
+//	String classPath = makeClassPathFromSimpleStructure("somePath");
+//
+//	assertEquals("somePath", classPath);
+//}
+//
+//private String makeClassPathFromSimpleStructure(String path) throws Exception {
+//	PageData data = root.getData();
+//	data.setContent("!path " + path);
+//	root.commit(data);
+//	crawler = root.getPageCrawler();
+//	crawler.setDeadEndStrategy(new MockingPageCrawler());
+//	WikiPage page = crawler.getPage(root, somePagePath);
+//	String classPath = builder.getClasspath(page);
+//	return classPath;
+//}
+//
+//public void testThatPathsWithSpacesGetQuoted() throws Exception {
+//	crawler.addPage(root, somePagePath, "!path Some File.jar");
+//	crawler = root.getPageCrawler();
+//	WikiPage page = crawler.getPage(root, somePagePath);
+//
+//	assertEquals("\"Some File.jar\"", builder.getClasspath(page));
+//
+//	crawler.addPage(root, somePagePath, "!path somefile.jar\n!path Some Dir/someFile.jar");
+//	assertEquals("somefile.jar" + pathSeparator + "\"Some Dir/someFile.jar\"", builder.getClasspath(page));
+//}
+//
+//public void testWildCardExpansion() throws Exception {
+//	try {
+//		makeSampleFiles();
+//
+//		String classPath = makeClassPathFromSimpleStructure("testDir/*.jar");
+//		assertHasRegexp("one\\.jar", classPath);
+//		assertHasRegexp("two\\.jar", classPath);
+//
+//		classPath = makeClassPathFromSimpleStructure("testDir/*.dll");
+//		assertHasRegexp("one\\.dll", classPath);
+//		assertHasRegexp("two\\.dll", classPath);
+//
+//		classPath = makeClassPathFromSimpleStructure("testDir/one*");
+//		assertHasRegexp("one\\.dll", classPath);
+//		assertHasRegexp("one\\.jar", classPath);
+//		assertHasRegexp("oneA", classPath);
+//
+//		classPath = makeClassPathFromSimpleStructure("testDir/**.jar");
+//		assertHasRegexp("one\\.jar", classPath);
+//		assertHasRegexp("two\\.jar", classPath);
+//		assertHasRegexp("subdir(?:\\\\|/)sub1\\.jar", classPath);
+//		assertHasRegexp("subdir(?:\\\\|/)sub2\\.jar", classPath);
+//	} finally {
+//		deleteSampleFiles();
+//	}
+//}
